36733065f6aa094ac77831e6ec8a7bc161974470
isma <ismaelmed500@gmail.com>
2025-09-10 16:49:28 -0300

    Commit retroactivo del 2019, culiao

--- app.py
+++ app.py
@@ -117,5 +117,5 @@ def about():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", debug=True, port=int(os.environ.get("PORT", 5000)))
 
